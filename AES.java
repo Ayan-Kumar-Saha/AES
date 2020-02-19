145e08d86e899c9ac66c5b489468cd8d46b35d90
Ayan Kumar Saha <ayankumarsaha96@gmail.com>
2020-02-20 00:06:05 +0530

    First commit

---
+++ AES.java
@@ -0,0 +1,92 @@
+public class AES {
+
+    private String secretKey;
+
+    private String[] roundKeys = new String[10];
+
+
+    public void createSession() {
+
+        generateSecretKey();
+
+        scheduleKey();
+    }
+
+    public void generateSecretKey() {
+
+        secretKey = null;
+    }
+
+    private void scheduleKey() {
+
+        for (int i = 0 ; i < 10 ; i++) {
+
+            roundKeys[i] = null;
+        }
+    }
+
+
+    public String encrypt(String plaintext) { 
+
+        String encryptedMsgfromInitialRound = initialRound(plaintext);
+
+        String encryptedMsgfromMainRound = mainRound(encryptedMsgfromInitialRound);
+
+        String finalEncryptedMessage = finalRound(encryptedMsgfromMainRound);
+
+        return finalEncryptedMessage;
+    }
+
+    private String initialRound(String plaintext) {
+
+        return addRoundKey(plaintext, secretKey);
+    }
+
+    private String mainRound(String encryptedMsgfromInitialRound) {
+
+        String roundData = encryptedMsgfromInitialRound;
+
+        String subByteData, shiftRowsData, mixColumnData;
+
+        for(int i = 0 ; i < 9 ; i++ ) {
+
+            subByteData = subBytes(roundData);
+
+            shiftRowsData = shiftRows(subByteData);
+
+            mixColumnData = mixColumns(shiftRowsData);
+
+            roundData = addRoundKey(mixColumnData, roundKeys[i]);
+
+        }
+
+        return roundData;
+    }
+
+    private String finalRound(String encryptedMsgfromMainRound) {
+
+        String subByteData = subBytes(encryptedMsgfromMainRound);
+
+        String shiftRowsData = shiftRows(subByteData);
+
+        String finalData = addRoundKey(shiftRowsData, roundKeys[9]);
+
+        return finalData;
+    }
+
+    private String subBytes(String message) { return null; }
+
+    private String shiftRows(String message) { return null; }
+
+    private String mixColumns(String message) { return null; }
+
+    private String addRoundKey(String message, String roundKey) { return null; }
+
+ 
+
+
+
+   public String decrypt(String message) { return null; }
+
+    
+}
---
+++ Algorithm.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Algorithm {
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String message = null;
+
+        int choice = 0;
+
+        do {
+
+            System.out.println("\n\n--MAIN MENU--");
+            System.out.println("1. Encrypt a message");
+            System.out.println("2. Decrypt a message");
+            System.out.println("0. Quit");
+            System.out.print("\nEnter your choice: ");
+
+            choice = Integer.parseInt(br.readLine());
+
+            AES aes = new AES();
+
+            aes.
+
+            switch (choice) {
+
+                case 1: 
+
+                        message = inputData("\nEnter the message to be encrypted: ");
+
+                        String encryptedMessage = aes.encrypt(message);
+
+                        System.out.println("\nYour message after encryption: " + encryptedMessage);
+
+                        break;
+
+                case 2: 
+
+                        message = inputData("\nEnter the message to be decrypted: ");
+
+                        String decryptedMessage = aes.decrypt(message);
+
+                        System.out.println("\nYour message after encryption: " + decryptedMessage);
+
+                        break;
+
+                case 0:
+
+                default: 
+
+                        System.out.println("\nInvalid option choosen! Enter again.");
+                        break;
+            }
+
+        } while(choice != 0);
+
+    }
+
+    public static String inputData(String name) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(name);
+
+        return br.readLine();
+
+    }
+}
